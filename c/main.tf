3bbcd1ca115dfcb0fd47f5429e6bd3e552eebd8c
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2021-01-30 14:20:23 +0900

    add sample for for_each

---
+++ c/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    christmas-tree = {
+      source  = "cappyzawa/christmas-tree"
+      version = "0.3.1"
+    }
+  }
+}
+
+resource "christmas-tree" "tree" {
+
+  for_each = {
+    "key1" = "value1",
+    "key2" = "value2",
+    "key3" = "value3",
+    "key4" = "value4",
+  }
+
+  path = "/tmp/lab-tree-${each.key}"
+}
